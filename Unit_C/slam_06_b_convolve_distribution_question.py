470283c502d99cc7434a371c2a82a72bb06ba2ad
sudhakar <sudhakargoyal1@gmail.com>
2020-04-24 18:58:09 +0530

    convolution of 2 probability distribution function for robot's movement

---
+++ Unit_C/slam_06_b_convolve_distribution_question.py
@@ -0,0 +1,56 @@
+# Instead of moving a distribution, move (and modify) it using a convolution.
+# 06_b_convolve_distribution
+# Claus Brenner, 26 NOV 2012
+from pylab import plot, show, ylim
+from distribution import *
+
+def move(distribution, delta):
+    """Returns a Distribution that has been moved (x-axis) by the amount of
+       delta."""
+    return Distribution(distribution.offset + delta, distribution.values)
+
+def convolve(a, b):
+    """Convolve distribution a and b and return the resulting new distribution."""
+        # --->>> Put your code here.
+    if len(b.values) > len(a.values):
+        c = [0]*    ( 2*len(b.values) - len(a.values) )
+
+    else:
+        c = [0]*( 2*len(a.values) - len(b.values) )
+    k = [0.25, 0.5, 0.25]
+    for i in range(len(a.values)):
+        for j in range(0, 3):
+# =============================================================================
+#             print(i,j)
+#             print(a.values[i],"x",k[j])
+# =============================================================================
+            c[i+j] += a.values[i] * k[j] 
+    
+    result  = Distribution()
+    result.values = c
+    result.offset = a.offset + b.offset
+        
+    
+    return result # Replace this by your own result.
+
+
+if __name__ == '__main__':
+    arena = (0,200)
+
+    # Move 3 times by 20.
+    moves = [20] *7
+
+    # Start with a known position: probability 1.0 at position 10.
+    position = Distribution.unit_pulse(10)
+    plot(position.plotlists(*arena)[0], position.plotlists(*arena)[1],
+         linestyle='steps')
+
+    # Now move and plot.
+    for m in moves:
+        move_distribution = Distribution.triangle(m, 2)
+        position = convolve(position, move_distribution)
+        plot(position.plotlists(*arena)[0], position.plotlists(*arena)[1],
+             linestyle='steps')
+
+    ylim(0.0, 1.1)
+    show()
